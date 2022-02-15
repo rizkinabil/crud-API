6649cca5bfbd97934ea325ccca335f5a2a8e9c56
rizki nabil <rizki.nbl123@gmail.com>
2022-02-15 21:20:29 +0700

    add method get data by id

--- controller/controller-karyawan.js
+++ controller/controller-karyawan.js
@@ -26,6 +26,27 @@ module.exports = {
       connection.release();
     });
   },
+  getDataKaryawanByID(req, res) {
+    let id = req.params.id;
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      connection.query(
+        `
+            SELECT * FROM tabel_karyawan WHERE karyawan_id = ?;
+            `,
+        [id],
+        function (error, results) {
+          if (error) throw error;
+          res.send({
+            success: true,
+            message: 'Berhasil ambil data!',
+            data: results,
+          });
+        }
+      );
+      connection.release();
+    });
+  },
   addDataKaryawan(req, res) {
     let data = {
       karyawan_nama: req.body.nama,
--- routes/karyawan-route.js
+++ routes/karyawan-route.js
@@ -4,6 +4,9 @@ const { karyawan } = require('../controller');
 // get data karyawan
 router.get('/karyawan', karyawan.getDataKaryawan);
 
+// get data karyawan by id
+router.get('/karyawan/:id', karyawan.getDataKaryawanByID);
+
 // add data karyawan ke database
 router.post('/karyawan/add', karyawan.addDataKaryawan);
 
